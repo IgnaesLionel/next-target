e3dc33c2e632deeea26cd2e77da6efe4100da6cc
ignaesLionel <ignaes_lionel@hotmail.com>
2021-09-30 13:12:43 +0200

    .

--- components/ProRealTime/ProRealTime.js
+++ components/ProRealTime/ProRealTime.js
@@ -12,8 +12,8 @@ const ProRealTime = ({ data, spread, market }) => {
         cols="33"
         value={`
         defparam drawonlastbaronly = true
-        Voffset = 1*pipsize
-        gap = ${spread}
+        Voffset = 10*pipsize
+        gap = 0 
         DPP = ${data.DPP} + gap
         DR1 = ${data.DR1} + gap
         DS1 = ${data.DS1} + gap
@@ -45,36 +45,36 @@ MHigh = ${data.MHigh} + gap
 MLow = ${data.MLow} + gap
 MClose = ${data.MClose} + gap
  
-        DRAWTEXT("Pivot D ${market}.",barindex-2,DPP+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DR1 ${market}",barindex-2,DR1+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DR2 ${market}",barindex-2,DR2+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DR3 ${market}",barindex-2,DR3+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DS1 ${market}",barindex-2,DS1+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DS2 ${market}",barindex-2,DS2+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("DS3 ${market}",barindex-2,DS3+Voffset,SansSerif,Bold,16)coloured(0,255,0)
-        DRAWTEXT("Pivot W ${market}",barindex-2,WPP+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WR1 ${market}",barindex-2,WR1+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WR2 ${market}",barindex-2,WR2+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WR3 ${market}",barindex-2,WR3+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WS1 ${market}",barindex-2,WS1+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WS2 ${market}",barindex-2,WS2+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("WS3 ${market}",barindex-2,WS3+Voffset,SansSerif,Bold,16)coloured(0,0,255)
-        DRAWTEXT("Pivot M ${market}",barindex-2,MPP+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WR1 ${market}",barindex-2,MR1+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WR2 ${market}",barindex-2,MR2+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WR3 ${market}",barindex-2,MR3+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WS1 ${market}",barindex-2,MS1+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WS2 ${market}",barindex-2,MS2+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("WS3 ${market}",barindex-2,MS3+Voffset,SansSerif,Bold,16)coloured(255, 192, 203)
-        DRAWTEXT("Last Day High ${market}",barindex-2,DayHigh+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Day Low ${market}",barindex-2,DayLow+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Day Close ${market}",barindex-2,DayClose+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Week High ${market}",barindex-2,WHigh+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Week Low ${market}",barindex-2,WLow+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Week Close ${market}",barindex-2,WClose+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Month High ${market}",barindex-2,MHigh+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Month Low ${market}",barindex-2,MLow+Voffset,SansSerif,Bold,16)coloured(255,255,255)
-        DRAWTEXT("Last Month Close ${market}",barindex-2,MClose+Voffset,SansSerif,Bold,16)coloured(255,255,255)
+        DRAWTEXT("Pivot D ${market}.",barindex-2,DPP+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DR1 ${market}",barindex-2,DR1+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DR2 ${market}",barindex-2,DR2+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DR3 ${market}",barindex-2,DR3+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DS1 ${market}",barindex-2,DS1+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DS2 ${market}",barindex-2,DS2+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("DS3 ${market}",barindex-2,DS3+Voffset,SansSerif,Bold,10)coloured(0,255,0)
+        DRAWTEXT("Pivot W ${market}",barindex-2,WPP+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WR1 ${market}",barindex-2,WR1+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WR2 ${market}",barindex-2,WR2+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WR3 ${market}",barindex-2,WR3+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WS1 ${market}",barindex-2,WS1+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WS2 ${market}",barindex-2,WS2+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("WS3 ${market}",barindex-2,WS3+Voffset,SansSerif,Bold,10)coloured(0,0,255)
+        DRAWTEXT("Pivot M ${market}",barindex-2,MPP+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WR1 ${market}",barindex-2,MR1+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WR2 ${market}",barindex-2,MR2+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WR3 ${market}",barindex-2,MR3+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WS1 ${market}",barindex-2,MS1+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WS2 ${market}",barindex-2,MS2+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("WS3 ${market}",barindex-2,MS3+Voffset,SansSerif,Bold,10)coloured(255, 192, 203)
+        DRAWTEXT("Last Day High ${market}",barindex-2,DayHigh+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Day Low ${market}",barindex-2,DayLow+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Day Close ${market}",barindex-2,DayClose+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Week High ${market}",barindex-2,WHigh+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Week Low ${market}",barindex-2,WLow+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Week Close ${market}",barindex-2,WClose+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Month High ${market}",barindex-2,MHigh+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Month Low ${market}",barindex-2,MLow+Voffset,SansSerif,Bold,10)coloured(255,255,255)
+        DRAWTEXT("Last Month Close ${market}",barindex-2,MClose+Voffset,SansSerif,Bold,10)coloured(255,255,255)
 
         RETURN DPP COLOURED(0,227,0) STYLE(line,3) AS"fDPP", DS1 COLOURED(0,227,0) STYLE(line,3) AS"fDS1", DR1 COLOURED(0,227,0) STYLE(line,3) AS"fDR1", DS2 COLOURED(0,227,0) STYLE(line,3) AS"fDS2", DR2 COLOURED(0,227,0) STYLE(line,3) AS"fDR2", DS3 COLOURED(0,227,0) STYLE(line,3) AS"fDS3", DR3 COLOURED(0,227,0) STYLE(line,3) AS"fDR3", WS3 COLOURED(0,191,255) STYLE(line,3) AS"fWS3", WS2 COLOURED(0,191,255) STYLE(line,3) AS"fWS2", WS1 COLOURED(0,191,255) STYLE(line,3) AS"fWS1", WPP COLOURED(0,191,255) STYLE(line,3) AS"fWPP", WR1 COLOURED(0,191,255) STYLE(line,3) AS"fWR1", WR2 COLOURED(0,191,255) STYLE(line,3) AS"fWR2", WR3 COLOURED(0,191,255) STYLE(line,3) AS"fWR3", MPP COLOURED(255,20,147) STYLE(line,3) AS"fMPP" , MS1 COLOURED(255,20,147) STYLE(line,3) AS"fMS1" , MR1 COLOURED(255,20,147) STYLE(line,3) AS"fMR1" , MS2 COLOURED(255,20,147) STYLE(line,3) AS"fMS2" , MR2 COLOURED(255,20,147) STYLE(line,3) AS"fMR2" , MS3 COLOURED(255,20,147) STYLE(line,3) AS"fMS3" , MR3 COLOURED(255,20,147) STYLE(line,2) AS"fMR3", DayHigh COLOURED(255,255,255) STYLE(line,2) AS"DHigh  ${market}", DayLow COLOURED(255,255,255) STYLE(line,2) AS"DLow  ${market}", DayClose COLOURED(255,255,255) STYLE(line,2) AS"DClose  ${market}", WHigh COLOURED(255,255,255) STYLE(line,2) AS"WHigh  ${market}", WLow COLOURED(255,255,255) STYLE(line,2) AS"DLow  ${market}", WClose COLOURED(255,255,255) STYLE(line,2) AS"WClose  ${market}", MHigh COLOURED(255,255,255) STYLE(line,2) AS"MHigh  ${market}", MLow COLOURED(255,255,255) STYLE(line,2) AS"MLow  ${market}", MClose COLOURED(255,255,255) STYLE(line,2) AS"MClose  ${market}" 
         `}
