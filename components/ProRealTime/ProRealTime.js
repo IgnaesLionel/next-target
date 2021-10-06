3fe6e493845120bfc84e204258483e5cb18b0e4c
ignaesLionel <ignaes_lionel@hotmail.com>
2021-10-06 20:46:19 +0200

    .

--- components/ProRealTime/ProRealTime.js
+++ components/ProRealTime/ProRealTime.js
@@ -112,20 +112,8 @@ MClose = ${data.MClose} + gap
       return await navigator.clipboard.writeText(commentText);
   } else {
       // text area method
-      let textArea = document.createElement("textarea");
-      textArea.value = commentText;
-      // make the textarea out of viewport
-      textArea.style.position = "fixed";
-      textArea.style.left = "-999999px";
-      textArea.style.top = "-999999px";
-      document.body.appendChild(textArea);
-      textArea.focus();
-      textArea.select();
-      return new Promise((res, rej) => {
-          // here the magic happens
-          document.execCommand('copy') ? res() : rej();
-          textArea.remove();
-      });
+     console.log('test')
+      };
   }
 
 
